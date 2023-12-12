822e884056d037ef8f69a66483556f17d0855fda
Karolina Popławska <karolinaapoplawska@gmail.com>
2023-12-12 23:00:42 +0100

    Update [...404].tsx
    not found

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -7,6 +7,7 @@ export default function NotFound() {
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
       <h1>Page Not Found</h1>
+      <h1>Page Not Found</h1>
       <p>
         Visit{" "}
         <a href="https://start.solidjs.com" target="_blank">
